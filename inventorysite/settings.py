52ed9ab2f7cb8c1038f2c88d2e112537fcf4ef6b
Winner <winnerbrown9@gmail.com>
2024-12-05 14:10:25 -0800

    localhost

--- inventorysite/settings.py
+++ inventorysite/settings.py
@@ -25,7 +25,7 @@ if ENVIRONMENT == 'development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'inventorysite.onrender.com',]
 
 
 # Application definition
